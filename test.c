e7990886982e0326660924972b749fca74d7b6fd
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-02 03:45:51 +0700

    .

--- test.c
+++ test.c
@@ -11,22 +11,32 @@
 
 #define PAD_MUX_1     0x00000000
 volatile GPIO_TypeDef *GPIO = (volatile GPIO_TypeDef *)ARCHI_GPIO_ADDR;
+
+void delay_spin(uint32_t cnt) {
+  while (cnt--) {
+    __asm__ volatile ("nop");
+  }
+}
+
 int main()
 {
-  // Map LED3 to GPIO11
-  ARCHI_WRITE(ARCHI_APB_SOC_CTRL_ADDR, APB_SOC_PADFUN0_OFFSET + PAD_MUX_1, 1UL << (11 * 2)); 
-  // Config GPIO11 as OUTPUT
-  ARCHI_WRITE(ARCHI_GPIO_ADDR, GPIO_PADDIR_OFFSET, 1UL << 11);
-  // Turn on GPIO11 Clock
-  ARCHI_WRITE(ARCHI_GPIO_ADDR, GPIO_GPIOEN_OFFSET, 1UL << 11);
+  // // Map LED3 to GPIO11
+  // ARCHI_WRITE(ARCHI_APB_SOC_CTRL_ADDR, APB_SOC_PADFUN0_OFFSET + PAD_MUX_1, 1UL << (11 * 2)); 
+  // // Config GPIO11 as OUTPUT
+  // ARCHI_WRITE(ARCHI_GPIO_ADDR, GPIO_PADDIR_OFFSET, 1UL << 11);
+  // // Turn on GPIO11 Clock
+  // ARCHI_WRITE(ARCHI_GPIO_ADDR, GPIO_GPIOEN_OFFSET, 1UL << 11);
 
-  // GPIO->PADDIR |= (1UL << 11);
-  // GPIO->GPIOEN |= (1UL << 11);
+  GPIO->PADDIR |= (1UL << 11);
+  GPIO->GPIOEN |= (1UL << 11);
 
   while (1) {
-    // GPIO->PADOUTSET |= (1UL << 11);
+    GPIO->PADOUT |= (1UL << 11);
+    delay_spin(1000000);
+    GPIO->PADOUT &= ~(1UL << 11);
+    delay_spin(1000000);
   // Turn on GPIO11
-  ARCHI_WRITE(ARCHI_GPIO_ADDR, GPIO_PADOUT_OFFSET, 1UL << 11);
+  // ARCHI_WRITE(ARCHI_GPIO_ADDR, GPIO_PADOUT_OFFSET, 1UL << 11);
   }
 
   return 0;
