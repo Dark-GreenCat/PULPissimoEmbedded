ea980a15e190a417a08bb4571d862d3830daca83
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-15 14:54:13 +0700

    [MODIFY] Add IO permission to GPIO_TypeDef

--- HardwareLayer/pulpissimo/peripheral.h
+++ HardwareLayer/pulpissimo/peripheral.h
@@ -12,20 +12,20 @@
   * @brief GPIO
   */
 typedef struct {
-    volatile uint32_t PADDIR;                // GPIO pad direction configuration register
-    volatile uint32_t GPIOEN;                // GPIO enable register
-    volatile uint32_t PADIN;                 // GPIO pad input value register
-    volatile uint32_t PADOUT;                // GPIO pad output value register
-    volatile uint32_t PADOUTSET;             // GPIO pad output set register
-    volatile uint32_t PADOUTCLR;             // GPIO pad output clear register
-    volatile uint32_t INTEN;                 // GPIO pad interrupt enable configuration register
-    volatile uint32_t INTTYPE0;              // GPIO pad interrupt type gpio 0 to 15 register
-    volatile uint32_t INTTYPE1;              // GPIO pad interrupt type gpio 16 to 31 register
-    volatile uint32_t INTSTATUS;             // GPIO pad interrupt status register
-    volatile uint32_t PADCFG0;               // GPIO pad pin 0 to 7 configuration register
-    volatile uint32_t PADCFG1;               // GPIO pad pin 8 to 15 configuration register
-    volatile uint32_t PADCFG2;               // GPIO pad pin 16 to 23 configuration register
-    volatile uint32_t PADCFG3;               // GPIO pad pin 24 to 31 configuration register
+    __IO volatile uint32_t PADDIR;                // GPIO pad direction configuration register
+    __IO volatile uint32_t GPIOEN;                // GPIO enable register
+    __I  volatile uint32_t PADIN;                 // GPIO pad input value register
+    __IO volatile uint32_t PADOUT;                // GPIO pad output value register
+    __IO volatile uint32_t PADOUTSET;             // GPIO pad output set register
+    __IO volatile uint32_t PADOUTCLR;             // GPIO pad output clear register
+    __IO volatile uint32_t INTEN;                 // GPIO pad interrupt enable configuration register
+    __IO volatile uint32_t INTTYPE0;              // GPIO pad interrupt type gpio 0 to 15 register
+    __IO volatile uint32_t INTTYPE1;              // GPIO pad interrupt type gpio 16 to 31 register
+    __I  volatile uint32_t INTSTATUS;             // GPIO pad interrupt status register
+    __IO volatile uint32_t PADCFG0;               // GPIO pad pin 0 to 7 configuration register
+    __IO volatile uint32_t PADCFG1;               // GPIO pad pin 8 to 15 configuration register
+    __IO volatile uint32_t PADCFG2;               // GPIO pad pin 16 to 23 configuration register
+    __IO volatile uint32_t PADCFG3;               // GPIO pad pin 24 to 31 configuration register
 } GPIO_TypeDef;
 
 
