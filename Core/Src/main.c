cc3c301bb73d82dee69501d0aeb9f7b2de115f14
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-15 21:58:35 +0700

    [MODIFY] Modify main.c to use the new GPIO HCL

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,10 +2,8 @@
 
 #define PAD_MUX_1     0x00000000
 
-#define LED_PIN           GPIO_PIN_11
-GPIO_HandleTypeDef hgpio_led;
-
-volatile GPIO_TypeDef *GPIO = (volatile GPIO_TypeDef *) ARCHI_GPIO_ADDR;
+#define LED_GPIO_Port     GPIOL
+#define LED_Pin           GPIO_PIN_11
 
 void delay_spin(uint32_t cnt) {
   while (cnt--) {
@@ -22,13 +20,13 @@ void HCL_Delay(uint32_t ms) {
 void MX_GPIO_Init(void) {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /*Configure GPIO pins : L11 */
-  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  /*Configure GPIO pins : LED */
+  GPIO_InitStruct.Pin = LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HCL_GPIO_Init(GPIOL, &GPIO_InitStruct);
+  HCL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
   
-  ARCHI_WRITE(ARCHI_APB_SOC_CTRL_ADDR, APB_SOC_PADFUN0_OFFSET, (1UL << (11 * 2)));
+  MODIFY_REG(*((volatile uint32_t*) (SOC_CONTROL_BASE + 0x00000010 + PAD_MUX_1)), (3UL << (11 * 2)), (1UL << (11 * 2)));
 }
 
 int main()
@@ -36,7 +34,7 @@ int main()
   MX_GPIO_Init();
 
   while (1) {
-    HCL_GPIO_TogglePin(&hgpio_led);
+    HCL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
     HCL_Delay(1000);
   }
 
