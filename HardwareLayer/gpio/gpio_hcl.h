9243217df63e410102f1ec0e72bafc13d8372190
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-15 14:31:24 +0700

    [ADD] Add bit manipulation macro to pulpissimo.h and add HCL_GPIO_Init(), HCL_GPIO_Deinit() to gpio_hcl.h

--- HardwareLayer/gpio/gpio_hcl.h
+++ HardwareLayer/gpio/gpio_hcl.h
@@ -1,14 +1,41 @@
 #ifndef __GPIO_HCL_H
-#define __GPIO_HCL_H
+#define __GPIO_HCL_HGPIO_TypeDef
 
-#include "pulpissimo_utils.h"
-#include "gpio_def.h"
+#include "gpio_hcl_def.h"
 
-void HCL_GPIO_Init(void);
+/* Exported functions --------------------------------------------------------*/ 
+/** @addtogroup GPIO_Exported_Functions GPIO Exported Functions
+  * @{
+  */
 
-void HCL_GPIO_Config(GPIO_HandleTypeDef* hgpio, GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
-void HCL_GPIO_WritePin(GPIO_HandleTypeDef* hgpio, GPIO_PinState PinState);
-GPIO_PinState HCL_GPIO_ReadPin(GPIO_HandleTypeDef* hgpio);
-void HCL_GPIO_TogglePin(GPIO_HandleTypeDef* hgpio);
+/** @addtogroup GPIO_Exported_Functions_Group1 Initialization/de-initialization functions 
+ *  @brief    Initialization and Configuration functions
+ * @{
+ */
+     
+/* Initialization and de-initialization functions *****************************/
+void              HCL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init);
+void              HCL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin);
+
+/**
+  * @}
+  */
+
+/** @addtogroup GPIO_Exported_Functions_Group2 IO operation functions 
+ * @{
+ */
+   
+/* IO operation functions *****************************************************/
+GPIO_PinState     HCL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+void              HCL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
+void              HCL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
 
 #endif
