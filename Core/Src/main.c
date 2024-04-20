88793449e9fca0fa91f30cf864b8722740553f6e
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-20 10:24:26 +0700

    [INIT] Init branch master-v2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,22 +1,6 @@
-#include "archi/chips/pulpissimo/apb_soc.h"
-#include "archi/chips/pulpissimo/memory_map.h"
-#include "archi/gpio/gpio_v3.h"
-#include "archi/utils.h"
-#include <stdio.h>
-#include <pulp.h>
 #include <stdint.h>
+
 #include <hal/gpio/gpio_v3.h>
-
-#include "gpio_def.h"
-#include "gpio_hcl.h"
-#include "pulpissimo_utils.h"
-
-#define PAD_MUX_1     0x00000000
-
-#define LED_PIN           GPIO_PIN_11
-GPIO_HandleTypeDef hgpio_led;
-
-volatile GPIO_TypeDef *GPIO = (volatile GPIO_TypeDef *) ARCHI_GPIO_ADDR;
 
 void delay_spin(uint32_t cnt) {
   while (cnt--) {
@@ -30,23 +14,17 @@ void HCL_Delay(uint32_t ms) {
   }
 }
 
-void MX_GPIO_Init(void) {
-  hgpio_led.Port = GPIO;
-  hgpio_led.Pin = LED_PIN;
-  
-  ARCHI_WRITE(ARCHI_APB_SOC_CTRL_ADDR, APB_SOC_PADFUN0_OFFSET, (1UL << (11 * 2)));
-  SET_BIT(hgpio_led.Port->PADDIR, hgpio_led.Pin);
-  SET_BIT(hgpio_led.Port->GPIOEN, hgpio_led.Pin);
-}
+extern void delay_us(uint32_t us);
 
 int main()
 {
-  MX_GPIO_Init();
+  hal_gpio_set_dir(1 << 11, 1);
+  hal_gpio_en_set(1 << 11);
 
-  while (1) {
-    HCL_GPIO_TogglePin(&hgpio_led);
-    HCL_Delay(1000);
+  while(1) {
+    hal_gpio_set_pin_value(11, 1);
+    delay_us(500000);
+    hal_gpio_set_pin_value(11, 0);
+    delay_us(500000);
   }
-
-  return 0;
 }
